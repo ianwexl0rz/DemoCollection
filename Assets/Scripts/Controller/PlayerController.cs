00e80fc9c1a0283cd6aadf522c95e582992c9a68
William Gibbons <obscurialoke@gmail.com>
2017-05-14 20:12:23 -0700

    added revised "GIRL" character. renamed her components to be easier to manage. Added improved rig and a walking animation. Attached movement script and camera controller. Hold shift to initiate normal walk animation otherwise it’ll blend funny. There should be an idle, walk, and run  (run supposed to play when you hit shift). But I only made a walk in order to test it, which plays when you hold shift. I will add more animations later!

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //animations are called in unity blend tree so don't need to be named within this script. they will be triggered based on movement speed
+    public float walkSpeed = 4;
+    public float runSpeed = 20;
+
+    public float turnSmoothTime = 0.2f; //time it takets from angle to go from current value to target value
+    float turnSmoothVelocity; //ref
+
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+    float currentSpeed;
+
+    Animator animator;
+	Transform cameraT;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+		cameraT = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //keyboard input vector2
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        //turns input vector into direction
+        Vector2 inputDir = input.normalized;
+
+        //only calculate direction if input direction is not 00
+        if (inputDir != Vector2.zero)
+        {
+            //charactor rotation using trigonometry. finding angle of rotation (theta). arctan ? 
+			float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime); //returns angle in radians but we want it in degrees, so we multiply by radians to degrees
+
+        }
+        bool running = Input.GetKey(KeyCode.LeftShift); //hold down shift key to run
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude; //if we are running the speed = runspeed outwise speed = walk speed. *magnitude determines that if there's no input, magnitude will be 0 so character wont move at 0
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothVelocity);
+        //move character
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        //control speed percent in animator so that character walks or runs depending on speed
+        float animationSpeedPercent = ((running) ? 1:.5f) *inputDir.magnitude;     //if running = 1 otherwise .5.  ? and : seems to be shorthand for if/else
+        //reference for animator
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Controller/ThirdPersonCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+    public float mouseSensitivity = 10;
+    public Transform target;
+    public float dstFromTarget = 2;
+	public Vector2 pitchMinMax = new Vector2(-40,85);
+
+	public float rotationSmoothTime = .12f;
+	Vector3 rotationSmoothVelocity;
+	Vector3 currentRotation;
+    float yaw; //rotation on the y axis
+    float pitch; //rotation on the x axis
+    
+
+	
+	// Update is called once per frame
+	void Update () {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity; //+= would invert
+		pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+
+		currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmoothVelocity, rotationSmoothTime);
+		transform.eulerAngles = currentRotation;
+
+        transform.position = target.position - transform.forward * dstFromTarget;
+	}
+}
