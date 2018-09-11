cb60696320bf81f38396e074e28cc1fff2c1d7e7
Ian Wexler <ianwexler@gmail.com>
2018-09-11 15:22:15 -0700

    Revert "Migrated to the new HD Render Pipeline. Set up new materials and post-effects."
    This reverts commit 9d837c0cf9135500ee39681abaad377c94ec0f7d.

--- Assets/Scripts/Camera/ThirdPersonCamera.cs
+++ Assets/Scripts/Camera/ThirdPersonCamera.cs
@@ -19,6 +19,7 @@ public class ThirdPersonCamera : MonoBehaviour
 	private Vector3 rotation;
 	private Vector3 rotationVelocity;
 
+	private float distance;
 	private float lockOnDistance = 0f;
 	private float distanceVel;
 	private Vector3 switchVelocity;
@@ -36,6 +37,7 @@ public class ThirdPersonCamera : MonoBehaviour
 
 	public void Setup()
 	{
+		distance = normalDistance;
 		player = oldPlayer = GameManager.I.activePlayer;
 		lastTargetPos = player.transform.position;
 	}
